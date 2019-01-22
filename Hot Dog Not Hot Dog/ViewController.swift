1d3359ffd151c75bbb28b37d9038a79189d31bbd
Deepak Balaji <deepak_ucf@icloud.com>
2019-01-21 21:50:52 -0500

    Initial Commit

---
+++ Hot Dog Not Hot Dog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hot Dog Not Hot Dog
+//
+//  Created by Deepak Balaji on 1/21/19.
+//  Copyright © 2019 Deepak Balaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
